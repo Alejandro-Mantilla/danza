ecd4813d2fa3c7bc41af900290bf455ba2e7ef09
AleManCar <alemancar0511@gmail.com>
2024-08-05 20:43:48 -0500

    Inicializamos Git

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function initMap() {
+    var location = {lat: -34.397, lng: 150.644};
+    var map = new google.maps.Map(document.getElementById('mapa'), {
+        zoom: 8,
+        center: location
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+
+window.onload = initMap;
